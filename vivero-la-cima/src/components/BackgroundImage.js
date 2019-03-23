d95c27e9646e2d123876881836c5e957e1473e37
Paulo <paulozamora2000@gmail.com>
2019-03-22 19:25:51 -0600

    tweaking hero quality

--- vivero-la-cima/src/components/BackgroundImage.js
+++ vivero-la-cima/src/components/BackgroundImage.js
@@ -8,7 +8,7 @@ const BackgroundSection = ({ children, className, src }) => (
       query {
         hero: file(relativePath: { eq: "hero.jpg" }) {
           childImageSharp {
-            fluid(quality: 55, maxWidth: 1920) {
+            fluid(quality: 65, maxWidth: 1920) {
               ...GatsbyImageSharpFluid
             }
           }
